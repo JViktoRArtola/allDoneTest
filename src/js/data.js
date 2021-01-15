e8ead82ebf56b0da215bce191f4bda43acacfdfd
Juan Víctor Artola Romero <48799012+JViktoRArtola@users.noreply.github.com>
2021-01-15 01:57:13 -0500

    Change data add more information

---
+++ src/js/data.js
@@ -0,0 +1,25 @@
+export const navsItems = ['Task', 'Notes', 'People', 'Feed'];
+export const tasks = [
+  [
+    'Open AllDone Test',
+    '#important',
+    '@Victor',
+    'https://www.google.com',
+    'victor@gmail.com',
+  ],
+  [
+    'Do the Test',
+    '#important',
+    '@Victor',
+    'https://www.google.com',
+    'victor@gmail.com',
+  ],
+  [
+    'Send the Test',
+    '#important',
+    'to',
+    '@Carmona',
+    'https://www.google.com',
+    'victor@gmail.com',
+  ],
+];
